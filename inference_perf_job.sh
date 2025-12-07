359ccbad9e64b207970ea6a940b1eefe3545070b
Syeda Anjum <104458278+syeda-anjum@users.noreply.github.com>
2025-12-06 20:38:12 -0600

    Update inference_perf_job.sh

--- inference_perf_job.sh
+++ inference_perf_job.sh
@@ -1,6 +1,9 @@
+gcloud projects add-iam-policy-binding <project> \
+    --member "principal://iam.googleapis.com/projects/###/locations/global/workloadIdentityPools/.svc.id.goog/subject/ns/default/sa/" \
+    --role "roles/storage.objectUser"
+
 kubectl describe configmaps inference-perf-config
-
-
+# Rerun job
 kubectl delete job inference-perf && kubectl delete configmap inference-perf-config
 
 
